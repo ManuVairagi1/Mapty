1215b99de1bb8977c14a9595c2166c2008d96361
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-06-17 16:52:49 +0530

    Neji

--- app.js
+++ app.js
@@ -15,46 +15,70 @@ const months = [
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form-input--type");
-const inputDistance = document.querySelector(".form-input--distance");
-const inputDuration = document.querySelector(".form-input--duration");
-const inputCadence = document.querySelector(".form-input--cadence");
-const inputElevation = document.querySelector(".form-input--elevation");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+let map;
+let mapEvent;
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              maxHeight: 100,
-              autoClose: true,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("Hustle is the new alpha")
-          .openPopup();
+      map.on("click", function (mapE) {
+        form.classList.remove("hidden");
+        inputDistance.focus();
+        mapEvent = mapE;
       });
     },
     function () {
       alert("cannot get the location");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //clearing input fields;
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      "";
+
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("workout")
+    .openPopup();
+});
+
+//changing elevation gain to cadence
+
+inputType.addEventListener("change", function () {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
