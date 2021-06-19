c10d1081dd8c60c151e71772b4e19ed9c76ece0b
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-06-19 11:05:41 +0530

    hiruzen

--- app.js
+++ app.js
@@ -113,6 +113,14 @@ class App {
   _newWorkout(e) {
     e.preventDefault();
 
+    const validInputs = function (...inputs) {
+      inputs.every((inp) => Number.isFinite(inp));
+    };
+
+    const allPositive = function (...inputs) {
+      inputs.every((inp) => inp > 0);
+    };
+
     //Get data from form
     const type = inputType.value;
     const distance = +inputDistance.value;
@@ -125,9 +133,8 @@ class App {
       const cadence = +inputCadence.value;
 
       if (
-        !Number.isFinite(distance) ||
-        !Number.isFinite(duration) ||
-        !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
       )
         return alert("Please submit a positive number");
     }
@@ -135,6 +142,12 @@ class App {
     //If activity cycling , create cycling object
     if (type === "cycling") {
       const elevation = +inputElevation.value;
+
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert("Please submit a positive number");
     }
     //Add new object to the workout array
 
